f022526dc2e4285090391264755f61eaea0752e0
vbalogh-ultinous <vbalogh@ultinous.com>
2020-03-17 14:56:34 +0000

    optimized forward time computation

--- forward_time.py
+++ forward_time.py
@@ -5,9 +5,11 @@ import time
 from yolact import Yolact 
 from data import cfg, set_cfg, set_dataset
 from utils.functions import SavePath
+from utils.augmentations import FastBaseTransform
+import gc
 
-img_sizes = [(640, 360), (1280, 720)] 
-batch_sizes = [1,2,4,8,16]
+img_sizes = [(1280, 720)]#[(640, 360), (1280, 720)] 
+batch_sizes = [8]#[1,2, 4,8]
 test_num = 100
 model = 'weights/yolact_plus_base_54_800000.pth' #'weights/yolact_base_54_800000.pth''weights/yolact_plus_base_54_800000.pth'
 
@@ -29,25 +31,26 @@ net.cuda()
 print('Model: {}'.format(model))
 print('Test number: {}'.format(test_num))
 
-for img_size in img_sizes:
-    print()
-    print('Input size: {}x{}'.format(img_size[0], img_size[1]))
-    
-    for batch_size in batch_sizes:
-        print('\tBatch size: {}'.format(batch_size))
-        x = torch.zeros((batch_size, 3, img_size[0], img_size[1]))
-        x.cuda()
-        cost = 0
+with torch.no_grad():
+    for img_size in img_sizes:
+        print()
+        print('Input size: {}x{}'.format(img_size[0], img_size[1]))
         
-        for i in range(test_num):
-            torch.cuda.synchronize()
-            t0 = time.time()
-            y = net(x)
-            torch.cuda.synchronize()
-            t1 = time.time()
-            cost += t1 - t0 # seconds
+        for batch_size in batch_sizes:
+            print('\tBatch size: {}'.format(batch_size))
+            #x = torch.zeros((batch_size, 3, img_size[0], img_size[1])).cuda()
+            cost = 0
+            
+            for i in range(test_num):
+                gc.collect()
+                torch.cuda.synchronize()
+                t0 = time.time()
+                net( torch.zeros((batch_size, 3, img_size[0], img_size[1])).cuda() )
+                torch.cuda.synchronize()
+                t1 = time.time()
+                cost += t1 - t0 # seconds
 
-        cost = cost / test_num
-        fps = (test_num * batch_size) / cost
+            cost = cost / test_num
+            fps = (test_num * batch_size) / cost
 
-        print('\tAverage spend time {:.2f}ms, {:.2f} sec \n\tfps: {}'.format(cost*1000, cost, fps))
+            print('\tAverage spend time {:.2f}ms, {:.2f} sec \n\tfps: {}'.format(cost*1000, cost, fps))
