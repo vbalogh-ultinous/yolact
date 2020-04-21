aa44e2bcc3161f23ba4838135f442cc6b3b741ab
vbalogh-ultinous <vbalogh@ultinous.com>
2020-04-21 15:18:21 +0200

    updated config sbd itersize

--- data/config.py
+++ data/config.py
@@ -873,7 +873,7 @@ yolact_plus_base_3x_config = yolact_plus_base_config.copy({
 })
 
 yolact_plus_person_3x_config = yolact_plus_person_config.copy({
-    'max_iter': 356000,  # ~ 800k / 1.85 / 1.3
+    'max_iter': 333000,  # ~ 800k / 1.85 / 1.3
     'lr_steps': (int(280000 / 1.85 / 1.3), int(600000 / 1.85 / 1.3), int(700000 / 1.85 / 1.3), int(750000 / 1.85 / 1.3)),
     'crowd_iou_threshold': 0.5,  # originally set to 0.7
 })
@@ -894,7 +894,7 @@ yolact_plus_person_3x_16_config =  yolact_plus_person_3x_config.copy({
 })
 
 yolact_plus_person_3x_16_coco_sbd_config =  yolact_plus_person_3x_16_config.copy({
-    'max_iter': 333000,  # ~ 800k / 1.73 / 1.3
+    'max_iter': 356000,  # ~ 800k / 1.73 / 1.3
     'lr_steps': (int(280000 / 1.73 / 1.3), int(600000 / 1.73 / 1.3), int(700000 / 1.73 / 1.3), int(750000 / 1.73 / 1.3)),
     'crowd_iou_threshold': 0.5,  # originally set to 0.7
     'dataset': coco_sbd_person_dataset,
